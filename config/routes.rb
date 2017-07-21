db68d526cf5f28dadfa7660daa44cd300aeca3a6
YusukeHaneda <yusukehaneda0824@gmail.com>
2017-07-21 10:02:02 +0000

    Edit routes.rb for the root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -51,6 +54,5 @@ Rails.application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #     resources :products  #   end
 end
